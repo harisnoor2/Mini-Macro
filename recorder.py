d840b182f818afde971e743aec579e8536b46f14
Haris <harisnoor465@gmail.com>
2024-09-28 19:10:04 -0600

    File saving done

--- recorder.py
+++ recorder.py
@@ -16,13 +16,29 @@ class Recorder:
         self.mouse_controller = mouse.Controller()
         self.keyboard_controller = keyboard.Controller()
         self.button_dict = {Button.left:"left_click", Button.right:"right_click", Button.middle:"middle_click", Button.x1:"side1_click", Button.x2:"side2_click"}
+        self.selected_file = None
 
-
+    def is_recording(self):
+        return self.recording
+    """
+    def is_recording(self, func):
+        def wrapper():
+            if self.recording:
+                func()
+        return wrapper
+    
+    def is_playbacking(self, func):
+        def wrapper():
+            if self.playback:
+                func()
+        return wrapper
+    """
     def start_recording(self):
+        self.events = []
         self.recording = True
         self.mouse_listener = mouse.Listener(on_move=self.on_move, on_click=self.on_click, on_scroll=self.on_scroll)
-        self.keyboard_listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
         self.mouse_listener.start()
+        self.keyboard_listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
         self.keyboard_listener.start()
         self.recordtime = time()
 
@@ -30,57 +46,40 @@ class Recorder:
         self.recording = False
         self.mouse_listener.stop()
         self.keyboard_listener.stop()
-        self.mouse_listener = None
-        self.keyboard_listener = None
-        self.event_to_json()
-        self.events = []
+        #self.event_to_json()
 
-    def event_to_json(self):
-        with open('macro.json', 'w') as savefile:
-            #print(self.events)
-            json.dump(self.events, savefile, indent = 4)
+    def event_to_json(self, save_path = None):
+        if save_path:
+            with open(save_path, 'w') as save_file:
+                json.dump(self.events, save_file, indent=4)
+
+    def json_to_event(self, path):
+        with open(path, "r") as load_file:
+            try:
+                self.events = json.load(load_file)
+                return True
+            except:
+                return False
 
     def on_move(self, x, y):
         self.events.append({"type":"move", "time":time() - self.recordtime, "x":x, "y":y})
-        #x, y, time
-        #print('Pointer moved to {0}'.format(
-        #(x, y)))
-
+    
     def on_click(self, x, y, button, pressed):
         if button == Button.unknown:
             return
         self.events.append({"type":self.button_dict[button], "time":time() - self.recordtime, "x":x, "y":y, "pressed":pressed})
-        #print('{0} at {1}'.format(
-        #    'Pressed' if pressed else 'Released',
-        #    (x, y)))
-        #print(button, pressed)
-
+    
     def on_scroll(self, x, y, dx, dy):
         self.events.append({"type":"scroll", "time":time() - self.recordtime, "x":x, "y":y, "dx":dx, "dy":dy})
 
-        #print('Scrolled {0} at {1}'.format(
-        #    'down' if dy < 0 else 'up',
-        #   (x, y)))
-        #print(dx,dy)
-
     def on_press(self, key):
         _key = self.serialize_key(key)
-        print(type(_key))
         self.events.append({"type":"key_press", "time":time() - self.recordtime, "key":_key})
-        #try:
-            #print('alphanumeric key {0} pressed'.format(
-                #key.char))
-        #except AttributeError:
-            #print('special key {0} pressed'.format( 
-                #key))
+
 
     def on_release(self, key):
         _key = self.serialize_key(key)
-        print(type(_key))
-
         self.events.append({"type":"key_release", "time":time() - self.recordtime, "key":_key})
-        #print('{0} released'.format(
-            #key))
         #if key == keyboard.Key.esc:
             # Stop listener
             #return False
@@ -92,9 +91,9 @@ class Recorder:
         self.playback = True
         self.ui_window.record_button_switch(False)
         self.ui_window.playback_button_switch(False)
-        
-        with open("macro.json", "r") as savefile:
-            macro_events = json.load(savefile)
+        macro_events = self.events
+        #with open("macro.json", "r") as savefile:
+            #macro_events = json.load(savefile)
 
         reverse_button_dict = {value:key for key,value in self.button_dict.items()}
         start_time = time()
@@ -117,29 +116,21 @@ class Recorder:
                 self.mouse_controller.scroll(event["dx"], event["dy"])
             elif event["type"] == "key_press":
                 key = self.deserialize_key(event["key"])
-                print(key)
                 self.keyboard_controller.press(key)
             elif event["type"] == "key_release":
                 key = self.deserialize_key(event["key"])
-                print(type(key))
-    
                 self.keyboard_controller.release(key)
 
         self.playback = False
         self.ui_window.record_button_switch(True)
         self.ui_window.playback_button_switch(True)
+
     def serialize_key(self, key):
         if "Key." in str(key):
             return str(key)
-        return key.char
+        return self.keyboard_listener.canonical(key).char
 
     def deserialize_key(self, key):
         if "Key." in key:
             return eval(key)
         return key
-
-    def is_recording(self):
-        return self.recording
-    
-    def is_playbacking(self):
-        return self.playback
--- ui.py
+++ ui.py
@@ -1,22 +1,76 @@
 import tkinter as tk
 from recorder import Recorder
-
+from tkinter import ttk, messagebox, filedialog
 class Window:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Mini Macro")
         self.root.geometry("265x75")
         self.root.resizable(False,False)
-
+        self.saved_file_path = None
         self.recorder = Recorder(self)
 
         self.record_button = tk.Button(self.root, text = "Record", command = self.start_recording)
-        #self.record_button.bind("<ButtonPress-1>", self.start_recording)
         self.record_button.pack()
-        
 
-        self.playback_button = tk.Button(self.root, text = "Playback", command = self.start_playback)
+        self.playback_button = tk.Button(self.root, text = "Playback", command = self.start_playback, state="disabled")
         self.playback_button.pack()
+
+        menu_bar = tk.Menu(self.root)
+
+        file_menu = tk.Menu(tearoff=0)
+        file_menu.add_command(label="Open", command=self.open_file)
+        file_menu.add_command(label="Save", command=self.save_file)
+
+        menu_bar.add_cascade(label="File", menu=file_menu)
+
+        hotkey_menu = tk.Menu(menu_bar, tearoff=0)
+        hotkey_menu.add_command(label="Recording", command=self.copy_text)
+        hotkey_menu.add_command(label="Playback", command=self.paste_text)
+
+        menu_bar.add_cascade(label="Hotkeys", menu=hotkey_menu)
+
+        playback_menu = tk.Menu(menu_bar, tearoff=0)
+        playback_menu.add_command(label="Options", command=self.about_app)
+
+        menu_bar.add_cascade(label="Playback", menu=playback_menu)
+
+        self.root.config(menu=menu_bar)
+
+    def open_file(self):
+        file_path = filedialog.askopenfilename(
+            title="Select a File",
+            filetypes=(("Text Files", "*.json"), ("All Files", "*.*"))
+        )
+        print(file_path)
+        if file_path:
+            if self.recorder.json_to_event(file_path):
+                self.playback_button.config(state="active")
+                self.saved_file_path = file_path
+                self.playback_button_switch(True)
+
+
+    def save_file(self):
+        file_path = filedialog.asksaveasfilename(
+            title="Save a File",
+            defaultextension=".json",
+            filetypes=(("Text Files", "*.json"), ("All Files", "*.*"))
+        )
+        if file_path:
+            self.saved_file_path = file_path
+            self.recorder.event_to_json(self.saved_file_path)
+
+    def copy_text(self):
+        messagebox.showinfo("Info", "Text copied!")
+
+    def paste_text(self):
+        messagebox.showinfo("Info", "Text pasted!")
+
+    def about_app(self):
+        messagebox.showinfo("About", "This is a sample application.")
+
+    def exit_app(self):
+        self.root.quit()
 
     def run(self): 
         self.root.mainloop()
@@ -25,7 +79,9 @@ class Window:
         if self.recorder.is_recording():
             self.record_button.config(text="Record")
             self.recorder.stop_recording()
+            self.playback_button_switch(True)
         else:
+            self.saved_file_path = None
             self.record_button.config(text="Stop")
             self.recorder.start_recording()
 

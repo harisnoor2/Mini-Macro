91e1e7b18607543c7e7e1f96a294ed09f8680666
harisnoor2 <156768148+harisnoor2@users.noreply.github.com>
2024-10-03 18:37:31 -0600

    Delete src/test.py
    oops

--- src/test.py
+++
@@ -1,6 +0,0 @@
-from pynput import mouse, keyboard
-from pynput.mouse import Button
-
-button_dict = {Button.left:"left_click", Button.right:"right_click", Button.middle:"middle_click", Button.x1:"side1_click", Button.x2:"side2_click"}
-
-print(button_dict.keys())
